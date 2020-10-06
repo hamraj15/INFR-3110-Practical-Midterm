b2a7c7e6af30ba5b3cf2a4e6b19e7120a97123b1
hamraj15 <hamraj.rai@ontariotechu.net>
2020-10-06 12:46:07 -0400

    Initial Project Push w Character Controller Started

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    public GameObject player;
+    public float playerSpeed;
+    public GameObject playerCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();  
+    }
+
+    private void Move()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            player.transform.position += Vector3.forward * Time.deltaTime * playerSpeed;  
+        }
+    }
+
+    private void CameraMove()
+    {
+
+    }
+}
+
+
