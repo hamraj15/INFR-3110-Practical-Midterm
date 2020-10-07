d4586c23c122c54990052779df27e211f8487fb8
hamraj15 <43864629+hamraj15@users.noreply.github.com>
2020-10-07 00:46:10 -0400

    Player Control & Scene Switching

---
+++ Assets/Scripts/playerMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+
+    private CharacterController playerControl;
+    public GameObject playerCamera;
+    public float playerSpeed;
+
+    private Vector3 move;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControl = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+        CameraMove();
+    }
+
+    private void PlayerMove()
+    {
+        move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        playerControl.Move(move * Time.deltaTime * playerSpeed);
+
+        if (move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+    }
+
+    private void CameraMove()
+    {
+        playerCamera.transform.position += (move * Time.deltaTime * playerSpeed);
+    }
+}
---
+++ Assets/Scripts/sceneManagement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManagement : MonoBehaviour
+{
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadEnd()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
