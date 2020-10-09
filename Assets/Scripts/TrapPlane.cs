916b802f31e84576bedb93774a106bcca7eeff80
hamraj15 <43864629+hamraj15@users.noreply.github.com>
2020-10-09 01:30:08 -0400

    Trap, Moving Wall, Moving Platforms, Checkpoints

---
+++ Assets/Scripts/TrapPlane.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TrapPlane : MonoBehaviour
+{
+
+    public void OnTriggerEnter(Collider other)
+    {
+        gameObject.SetActive(false);
+    }
+}
